67d0e604a4dd6c5c660f65fd9e72030717d45ecd
Ankur Shukla <ashukla@thinkanalytics.com>
2020-12-24 12:40:36 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//setup vars
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [
+  {
+    id: 0,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81zBVMvSjNL._AC_UL200_SR200,200_.jpg",
+    title: "Midnight Sun",
+    author: "Stephenie Meyer",
+  },
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+    title: "A Promise Land",
+    author: "Barack Obama",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81-349iYbfL._AC_UL200_SR200,200_.jpg",
+    title: "Where the Crawdads Sing",
+    author: "Delia Owens",
+  },
+];
+
+function BookList() {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} bookProps={book} />;
+      })}
+    </section>
+  );
+}
+
+const Book = (props) => {
+  const { img, title, author } = props.bookProps;
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+// const Book1 = ({ img, title, author, children }) => {
+//   // de-structuring props as
+//   // const {img, title, author } = props;
+// ** 'children' is anything in the props between starting and ending tags of a component
+//   return (
+//     <article className="book">
+//       <img src={img} alt="" />
+//       <h1>{title}</h1>
+//       <h4>{author}</h4>
+//        {children}
+//     </article>
+//   );
+// };
+
+// const Author = () => {
+//   return (
+//        //  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+//       //   John Grisham
+//       // </h4>
+//   );
+// };
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
